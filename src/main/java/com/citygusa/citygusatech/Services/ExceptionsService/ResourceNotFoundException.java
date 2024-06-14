9b0f20dd362bf7aefb031504b14719de42f16d53
Wayster cruz de melo <57998761+waysterMelo@users.noreply.github.com>
2024-06-14 15:50:53 -0300

    Delete src directory

--- src/main/java/com/citygusa/citygusatech/Services/ExceptionsService/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.citygusa.citygusatech.Services.ExceptionsService;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
