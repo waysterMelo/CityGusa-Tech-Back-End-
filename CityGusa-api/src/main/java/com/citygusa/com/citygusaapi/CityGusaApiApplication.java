e4bdd82b88dc7a8a40c50b1993ac105ab9f0c6bd
Wayster Melo <wayster.melo@citygusa.local>
2024-06-14 07:14:54 -0300

    Init

---
+++ CityGusa-api/src/main/java/com/citygusa/com/citygusaapi/CityGusaApiApplication.java
@@ -0,0 +1,13 @@
+package com.citygusa.com.citygusaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityGusaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityGusaApiApplication.class, args);
+    }
+
+}
---
+++ CityGusa-api/src/test/java/com/citygusa/com/citygusaapi/CityGusaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citygusa.com.citygusaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityGusaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
